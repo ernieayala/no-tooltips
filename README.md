2fe46ecdcf994036a07ebb9e4dfec7c59480d8a0
Ernesto Ayala <teafortwo.ea@gmail.com>
2023-02-13 16:46:47 -0800

    no tooltips

---
+++ README.md
@@ -0,0 +1,8 @@
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/ernieayala/no-tooltips?style=flat-square)
+![GitHub Latest Release](https://img.shields.io/github/downloads/ernieayala/no-tooltips/latest/total?style=flat-square)
+![GitHub All Releases](https://img.shields.io/github/downloads/ernieayala/no-tooltips/total?label=total%20downloads&style=flat-square)
+
+# No Tooltips
+**Compatibility:** FoundryVTT 10.286
+
+Hides tooltips. Press 'alt' and hover to see a tooltip.
---
+++ js/no-tooltips.js
@@ -0,0 +1,75 @@
+const myHtml = document.getElementsByTagName('html');
+const myBody = document.getElementsByTagName('body');
+const moduleName = 'no-tooltips';
+let isPressed = false;
+let keyCode;
+
+function updateSettings(settings) {
+	const {
+		toggleKey
+	} = settings;
+}
+
+class noTooltipSettings {
+	static get settings() {
+		return mergeObject(
+			this.defaultSettings, game.settings.get(moduleName, 'settings')
+		);
+	}
+
+	static get defaultSettings() {
+		return {
+			toggleKey: 18
+		};
+	}
+}
+
+Hooks.once('init', () => {
+	myBody[0].classList.add('-no-tooltips');
+
+	game.settings.register(moduleName, 'settings', {
+		name: game.i18n.localize('no-tooltips.module-name'),
+		scope: 'world',
+		default: noTooltipSettings.defaultSettings,
+		type: Object,
+		config: false
+	});
+});
+
+Hooks.once('ready', () => {
+	updateSettings(game.settings.get(moduleName, 'settings'));
+
+	game.settings.register(moduleName, 'toggleKey', {
+		name: game.i18n.localize('no-tooltips.toggle-key.label'),
+		hint: game.i18n.localize('no-tooltips.toggle-key.hint'),
+		scope: 'user',
+		config: true,
+		default: 18,
+		type: String,
+		choices: {
+			16: game.i18n.localize('no-tooltips.toggle-key.option.shift'),
+			17: game.i18n.localize('no-tooltips.toggle-key.option.ctrl'),
+			18: game.i18n.localize('no-tooltips.toggle-key.option.alt')
+		}
+	});
+	const toggleKey = game.settings.get(moduleName, 'toggleKey');
+	keyCode = toggleKey;
+
+	console.log('%cErnie\'s No Tooltips', 'color: #ff0055;');
+});
+
+window.addEventListener('keydown', function (e) {
+	if (!e.repeat) {
+		if(e.keyCode == keyCode) {
+			isPressed = true;
+			myHtml[0].classList.add('-show-tooltips');
+		}
+	}
+}, false);
+
+window.addEventListener('keyup', function (e) {
+	if(isPressed) {
+		isPressed = false;
+		myHtml[0].classList.remove('-show-tooltips');
+	}
+}, false);
